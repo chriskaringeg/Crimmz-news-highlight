4a5d9c5f1fef7dfd80b520ec8d2eeb2531ee2b7a
chriskaringeg <ckaringe@gmail.com>
2019-02-06 17:05:04 +0300

    updated my app

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 # main = Blueprint('main', __name__)
 # from . import views, errors
 from config import config_options
+from flask_bootstrap import Bootstrap
+ 
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
